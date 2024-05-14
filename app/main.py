b3f5f69f9faf596b7678687319d56488214d5043
isaiasiotti18 <isaiasiottiprofissional@gmail.com>
2024-05-14 19:49:45 -0300

    main.py

--- app/main.py
+++ app/main.py
@@ -1,10 +1,24 @@
 from typing import Union
-
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.api import router as api_router
 
 app = FastAPI()
+
+# Configurar CORS
+origins = [
+  "http://localhost",
+  "http://localhost:8080",  # Ou o endereço do seu frontend
+]
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=origins,
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
+)
 
 app.include_router(api_router, prefix="/api/v1")
 
